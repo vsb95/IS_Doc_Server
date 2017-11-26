df8d1fc5d13c8feb9a179fc58af86b53772c6793
Vladimir <vsb95@mail.ru>
2017-11-26 20:39:23 +0600

    Добавлен механизм асинхронной обработки запросов

---
+++ IS_Doc_Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DatabaseFirst;
+using IS_Doc_Server.Classes.Log;
+using IS_Doc_Server.Classes.Net;
+
+namespace IS_Doc_Server
+{
+
+    class Program
+    {
+        const int port = 8888;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetMessage clientObject = new NetMessage(client);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.FatalException(ex);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+
+        }
+    }
+}
