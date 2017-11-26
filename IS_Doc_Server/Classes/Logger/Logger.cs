4c7aab3b371341f6006bbdd6322b44883c151f46
Vladimir <vsb95@mail.ru>
2017-11-26 20:35:32 +0600

    Добавлен логгер

---
+++ IS_Doc_Server/Classes/Logger/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace IS_Doc_Server.Classes.Log
+{
+    public static class Logger
+    {
+        private static NLog.Logger _logger = LogManager.GetCurrentClassLogger();
+        static Logger()
+        {
+        }
+        public static void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+        public static void Info(string message)
+        {
+            _logger.Info(message);
+        }
+        public static void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+        public static void Error(string message)
+        {
+            _logger.Error(message);
+        }
+        public static void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+        public static void WarnException(Exception exception)
+        {
+            _logger.Log(LogLevel.Warn, exception);
+        }
+        public static void FatalException(Exception exception)
+        {
+            _logger.Log(LogLevel.Fatal, exception);
+        }
+
+    }
+}
---
+++ IS_Doc_Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("IS_Doc_Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IS_Doc_Server")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3c3d1c1a-4045-4c54-9d1c-70f48cf804ae")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
